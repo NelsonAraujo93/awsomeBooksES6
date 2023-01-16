736b243d14bc8ea056a346406586ea75aceabcf1
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-01-16 15:47:41 -0500

    Add luxon time function and delete timer.js & add luxon.js

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ import { Book } from "./modules/book.js";
 import { renderListSection } from "./modules/listSection.js";
 import { renderAddSection } from "./modules/addSection.js";
 import { renderContactSection } from "./modules/contactSection.js";
+import { DateTime } from "./modules/luxon.js";
 // declare variables
 const mainContainer = document.getElementById('main');
 const navItems = document.getElementsByClassName('navitem');
 const date = document.getElementById('date');
-date.innerHTML = new Date().toLocaleString('en-US', {
-  month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true, second: 'numeric',
-});
+const stringDate = {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true, second: 'numeric',};
+let now = DateTime.now();
+const newDate = now.setLocale('en-US').toLocaleString(stringDate);
+date.innerHTML = newDate;
 
 const booksList = new BookList([]);
 booksList.fromLs();
